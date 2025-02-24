73af9e6874fcd6439bcc14d11ced8c827dbe5cb9
Thiago-vini-gr <Thiagaogamerrj@gmail.com>
2025-02-24 10:54:56 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Leitura dos valores inteiros
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        // Calcular a soma
+        int SOMA = A + B;
+
+        // Imprimir a soma com a formatação correta
+        Console.WriteLine($"SOMA = {SOMA}");
+    }
+}
